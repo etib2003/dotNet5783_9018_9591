32077a289e20e775ddbc728855ee97093b7fac7b
etib2003 <etibr2003@gmail.com>
2022-10-25 19:24:31 +0300

    open git

---
+++ dotNet5783_9018_9591/stage0/Program9018.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace stage0
+{ 
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9018();
+            Welcome9591();
+
+            Console.ReadKey();
+        }
+        private static void Welcome9018()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome9591();
+        
+    }
+}
+
+
+
+
+
+
+
+
