9a26e7ab46cfc4da5aef43eea676ac9c0d9222a9
Chavibomm <bombach@g.jct.ac.il>
2022-11-02 21:36:51 +0200

    a

--- dotNet5783_9018_9591/DalFacade/DO/Order.cs
+++ dotNet5783_9018_9591/DalFacade/DO/Order.cs
@@ -2,4 +2,11 @@
 
 public struct Order
 {
+    public int ID { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerAdress { get; set; }
+
+
 }
+ 
--- dotNet5783_9018_9591/DalFacade/DO/OrderItem.cs
+++ dotNet5783_9018_9591/DalFacade/DO/OrderItem.cs
@@ -2,4 +2,8 @@
 
 public struct OrderItem
 {
+    public int ProductID { get; set; }
+    public int OrderID { get; set; }
+    public double Price { get; set; }
+    public int Amount { get; set; }
 }
