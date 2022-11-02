09742b79cccf6638fdae198f1b0244d752425f23
etib2003 <etibr2003@gmail.com>
2022-11-02 21:40:16 +0200

    a

--- dotNet5783_9018_9591/DalFacade/DO/Product.cs
+++ dotNet5783_9018_9591/DalFacade/DO/Product.cs
@@ -1,6 +1,20 @@
-﻿namespace DO;
+﻿using System.ComponentModel;
+
+namespace DO;
 
 public struct Product
 {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    //public Category { get; set; }
+    public int InStock { get; set; }
+
+    public override string ToString() => $@"
+        Product ID={ID}: {Name}, 
+        category - {Category}
+    	Price: {Price}
+    	Amount in stock: {InStock}
+";
 
 }
