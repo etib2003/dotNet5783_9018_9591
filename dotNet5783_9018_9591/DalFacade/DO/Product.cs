e996972a7102312cc884349af711592d99c6a622
Chavibomm <bombach@g.jct.ac.il>
2022-11-03 18:22:12 +0200

    aa

--- dotNet5783_9018_9591/DalFacade/DO/Product.cs
+++ dotNet5783_9018_9591/DalFacade/DO/Product.cs
@@ -7,7 +7,7 @@ public struct Product
     public int ID { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
-    
+    public Category category { get; set; }
     public int InStock { get; set; }
     
 
