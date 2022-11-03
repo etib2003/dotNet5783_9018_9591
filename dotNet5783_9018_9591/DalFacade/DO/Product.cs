037732863aa3b0df0c2364ef9bfae76275d1b80d
Chavibomm <bombach@g.jct.ac.il>
2022-11-03 18:20:12 +0200

    יחע

--- dotNet5783_9018_9591/DalFacade/DO/Product.cs
+++ dotNet5783_9018_9591/DalFacade/DO/Product.cs
@@ -7,8 +7,9 @@ public struct Product
     public int ID { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
-    //public Category { get; set; }
+    
     public int InStock { get; set; }
+    
 
     public override string ToString() => $@"
         Product ID={ID}: {Name}, 
