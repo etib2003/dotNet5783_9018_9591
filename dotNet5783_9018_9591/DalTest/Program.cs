46a37407e31c32ddcd66cc6267ad76e0b82132a2
Chavibomm <bombach@g.jct.ac.il>
2022-11-06 18:17:11 +0200

    a

--- dotNet5783_9018_9591/DalTest/Program.cs
+++ dotNet5783_9018_9591/DalTest/Program.cs
@@ -1,4 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string choice ;
+string ch;
+do
+{ choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "0":
+            { 
+                break;
+            }
+        case "1":
+            {
+                ch = Console.ReadLine();
+                switch (ch)
+                {
+                    case 'c':
+                        Console.ReadLine();
 
 
+                }
+                
+            }
+        case "2":  
+             
+            {
+                ch = Console.ReadLine();
+                switch (ch)
+                {
+                     
+                }
+                 
+            }
+        case "3":
+            {
+                ch = Console.ReadLine();
+                switch (ch)
+                {
+                     
+                }
+            }
+            break;
+    }
+
+
+}
+while (choice != "0");
