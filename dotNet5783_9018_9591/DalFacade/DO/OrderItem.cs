a7e5eb5cb79b67d571edc4ebff47dc667d639062
etib2003 <etibr2003@gmail.com>
2022-11-10 19:37:28 +0200

    gv

--- dotNet5783_9018_9591/DalFacade/DO/OrderItem.cs
+++ dotNet5783_9018_9591/DalFacade/DO/OrderItem.cs
@@ -1,22 +1,39 @@
 ﻿namespace DO;
 
+/// <summary>
+/// a struct that links between the order and the product
+/// </summary>
 public struct OrderItem
 {
     /// <summary>
-    /// links between the order and the product
+    /// orderItem's unique id
     /// </summary>
-    public int seqNum { get; set; }//running number
+    public int seqNum { get; set; }
+    /// <summary>
+    /// orderItem's order id
+    /// </summary>
     public int OrderID { get; set; }
+    /// <summary>
+    /// orderItem's product barcode
+    /// </summary>
     public int ProductID { get; set; }
+    /// <summary>
+    /// orderItem's price
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// orderItem's amount to buy
+    /// </summary>
     public int Amount { get; set; }
-
+    /// <summary>
+    /// the orderItem's print method
+    /// </summary>
+    /// <returns>the way the orderItem is printed</returns>
     public override string ToString() => $@"
-        Identification number={seqNum} 
+        Identification number= {seqNum} 
         Order ID: {OrderID}
         Product ID: {ProductID}
     	Price: {Price}
     	Amount: {Amount}
 ";
-
 }
