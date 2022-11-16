335fa4ffa2ac476de0ffecb6e8b6f0acb7c75a32
etib2003 <etibr2003@gmail.com>
2022-11-16 20:22:33 +0200

    cfd

---
+++ dotNet5783_9018_9591/BL/BlApi/IOrder.cs
@@ -0,0 +1,19 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+
+internal interface IOrder
+{
+    public OrderForList GetOrderListForManager();
+    public Order GetOrderDetails(int orderID);
+    public Order UpdateOrderShip(int orderID);
+    public Order UpdateOrderDelivery(int orderID);
+    public Order TrakingOrder(int orderID);
+    public void UpdateOrder();//בונוס
+    public OrderForList GetOrderListForCustomer();
+}
