af3c56013768f20539087a535e67021e38199b8a
Chavibomm <bombach@g.jct.ac.il>
2022-11-16 20:22:20 +0200

    x

---
+++ dotNet5783_9018_9591/BL/BlApi/Icart.cs
@@ -0,0 +1,15 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+
+internal interface Icart
+{
+    public Cart AddProductToCart(Cart cart, int productId);
+    public Cart UpdateAmountOfProduct(Cart cart, int productId,int amount);
+    public void CommitOrder(Cart cart, string customerName, string customerEmail, string customerAdress);
+}
