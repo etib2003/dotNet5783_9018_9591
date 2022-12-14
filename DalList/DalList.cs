8e5aab67a721aa7154f90aee50cdf7d8cf262b75
Chavibomm <bombach@g>
2022-12-14 19:50:47 +0200

    19:50

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -5,9 +5,16 @@ namespace Dal;
 sealed internal class DalList : IDal
 {
     public static IDal Instance { get; } = new DalList();
-    private DalList() { }
-    public IProduct Product => new dalProduct();
-    public IOrder Order => new dalOrder();
-    public IOrderItem OrderItem => new dalOrderItem();
+
+    public IProduct Product { get; }
+    public IOrder Order { get; }
+    public IOrderItem OrderItem { get; }
+    private DalList()
+    {
+        Product = new dalProduct();
+        Order = new dalOrder();
+        OrderItem = new dalOrderItem();
+    }
+
 }
 
