284934f864e73db3e1c2f6e773f0ad1fd15dc889
Chavibomm <bombach@g>
2022-12-15 12:27:02 +0200

    12:26

--- DalXml/dalOrder.cs
+++ DalXml/dalOrder.cs
@@ -1,17 +1,68 @@
-﻿using System;
+﻿using DalApi;
+using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DalApi;
-using DO;
-namespace Dal;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 internal class dalOrder : IOrder
 {
+
+    string path = "orders.xml";
+    string configPath = "config.xml";
+    XElement ordersRoot;
+
+    private void LoadData()
+    {
+        try
+        {
+            if (File.Exists(path))
+                ordersRoot = XElement.Load(path);
+            else
+            {
+                ordersRoot = new XElement("orders");
+                ordersRoot.Save(path);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("product File upload problem" + ex.Message);
+        }
+    }
+
+    public dalOrder()
+    {
+        LoadData();
+    }
+
     public int Create(Order Or)
     {
-        throw new NotImplementedException();
+
+        //Read config file
+        XElement configRoot = XElement.Load(configPath);
+
+        int nextSeqNum = Convert.ToInt32(configRoot.Element("orderSeq").Value);
+        nextSeqNum++;
+        Or.Id = nextSeqNum;
+        //update config file
+        configRoot.Element("orderSeq").SetValue(nextSeqNum);
+        configRoot.Save(configPath);
+
+        XElement Id = new XElement("Id", Or.Id);
+        XElement CustomerName = new XElement("CustomerName", Or.CustomerName);
+        XElement CustomerEmail = new XElement("CustomerEmail", Or.CustomerEmail);
+        XElement CustomerAdress = new XElement("CustomerAdress", Or.CustomerAdress);
+        XElement OrderDate = new XElement("OrderDate", Or.OrderDate);
+        XElement ShipDate = new XElement("ShipDate", Or.ShipDate);
+        XElement DeliveryDate = new XElement("DeliveryDate", Or.DeliveryDate);
+
+        ordersRoot.Add(new XElement("Order", Id, CustomerName, CustomerEmail, CustomerAdress, OrderDate, ShipDate, DeliveryDate));
+        ordersRoot.Save(path);
+
+        return Or.Id;
     }
 
     public void Delete(int id)
