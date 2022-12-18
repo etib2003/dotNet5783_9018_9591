6d956a952f272cfc2cb90152314203007b0370c2
etib2003 <etibr2003@gmail.com>
2022-12-18 10:49:43 +0200

    10:50

--- DalXml/dalOrderItem.cs
+++ DalXml/dalOrderItem.cs
@@ -1,5 +1,6 @@
 ﻿using DalApi;
 using DO;
+using DocumentFormat.OpenXml.Office2010.Excel;
 using System.Xml.Linq;
 
 namespace Dal;
@@ -7,7 +8,9 @@ namespace Dal;
 internal class dalOrderItem : IOrderItem
 {
     string path = XmlTools.dir + "ordersItems.xml";
+    string configPath = "config.xml";
     XElement ordersItemsRoot;
+
 
     public dalOrderItem()
     {
@@ -31,24 +34,45 @@ internal class dalOrderItem : IOrderItem
             throw new Exception("product File upload problem" + ex.Message);
         }
     }
-    public int Create(OrderItem Or)
+    public int Create(OrderItem Oi)
     {
-        throw new NotImplementedException();
+        //Read config file
+        XElement configRoot = XElement.Load(configPath);
+        int.TryParse(configRoot.Element("orderItemSeq").Value, out int nextSeqNum);
+        nextSeqNum++;
+        Oi.Id = nextSeqNum;
+        //update config file
+        configRoot.Element("orderItemSeq").SetValue(nextSeqNum);
+        configRoot.Save(configPath);
+
+        XElement Id = new XElement("Id", Oi.Id);
+        XElement OrderID = new XElement("OrderID", Oi.OrderID);
+        XElement ProductID = new XElement("ProductID", Oi.ProductID);
+        XElement Price = new XElement("Price", Oi.Price);
+        XElement Amount = new XElement("Amount", Oi.Amount);
+
+        ordersItemsRoot.Add(new XElement("OrderItem", Id, OrderID, ProductID, Price, Amount));
+        ordersItemsRoot.Save(path);
+        return Oi.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        XElement OiElement = (from oi in ordersItemsRoot.Elements()
+                              where int.Parse(oi.Element("id").Value) == id
+                              select oi).FirstOrDefault() ?? throw new DalDoesNoExistException("Order Item");
+        OiElement.Remove();
+        ordersItemsRoot.Save(path);
     }
 
     public OrderItem Get(Func<OrderItem?, bool>? cond)
     {
-        return RequestAll(cond).Single() ?? throw new Exception();
+        return RequestAll(cond).Single() ?? throw new DalDoesNoExistException("Order Item");
     }
 
     public OrderItem GetById(int id)
     {
-        throw new NotImplementedException();
+        return Get(orderItem => orderItem?.Id == id);
     }
 
     public IEnumerable<OrderItem?> RequestAll(Func<OrderItem?, bool>? cond = null)
@@ -62,14 +86,21 @@ internal class dalOrderItem : IOrderItem
                                              Price = int.Parse(orderItem.Element("Price").Value),
                                              Amount = int.Parse(orderItem.Element("Amount").Value),
 
-
                                          }).Where(orderItem => cond is null ? true : cond(orderItem));
 
     }
 
-    public void Update(OrderItem Or)
+    public void Update(OrderItem Oi)
     {
-        throw new NotImplementedException();
+        XElement OiElement = (from oi in ordersItemsRoot.Elements()
+                                   where int.Parse(oi.Element("id").Value) == Oi.Id
+                                   select oi).FirstOrDefault() ?? throw new DalDoesNoExistException("Order Item");
+
+        OiElement.Element("OrderID").Value = Oi.OrderID.ToString();
+        OiElement.Element("ProductID").Value = Oi.ProductID.ToString();
+        OiElement.Element("Price").Value = Oi.Price.ToString();
+        OiElement.Element("Amount").Value = Oi.Amount.ToString();
+        ordersItemsRoot.Save(path);
     }
 }
 
